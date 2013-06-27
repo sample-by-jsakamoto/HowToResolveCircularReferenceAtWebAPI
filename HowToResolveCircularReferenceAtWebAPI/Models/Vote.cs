fe44129fc4c5781e6cc9d755c07ae8bb8cf29d1b
jsakamoto <junichi.jp@gmail.com>
2013-06-27 20:49:29 +0900

    Resolve circular reference for both 'application/json' and 'application/xml'.

--- HowToResolveCircularReferenceAtWebAPI/Models/Vote.cs
+++ HowToResolveCircularReferenceAtWebAPI/Models/Vote.cs
@@ -1,18 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Web;
 using Newtonsoft.Json;
 
 namespace HowToResolveCircularReferenceAtWebAPI.Models
 {
+    [DataContract]
     public class Vote
     {
+        [DataMember]
         public Guid VoteId { get; set; }
 
+        [DataMember]
         public Guid ThemeId { get; set; }
 
-        [JsonIgnore]
         public Theme Theme { get; set; }
 
         public Vote(Theme theme)

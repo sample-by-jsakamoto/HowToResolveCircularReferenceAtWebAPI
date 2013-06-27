000fbbff22f9ef8dd72244445d007201f01ee7c2
jsakamoto <junichi.jp@gmail.com>
2013-06-27 20:42:48 +0900

    Resolve circular reference for 'application/json'.

--- HowToResolveCircularReferenceAtWebAPI/Models/Vote.cs
+++ HowToResolveCircularReferenceAtWebAPI/Models/Vote.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Newtonsoft.Json;
 
 namespace HowToResolveCircularReferenceAtWebAPI.Models
 {
@@ -11,6 +12,7 @@ namespace HowToResolveCircularReferenceAtWebAPI.Models
 
         public Guid ThemeId { get; set; }
 
+        [JsonIgnore]
         public Theme Theme { get; set; }
 
         public Vote(Theme theme)
